0e91209401844e246cdbd1db68fbe18b0477e9f0
rostnrol <112760457+rostnrol@users.noreply.github.com>
2022-11-09 17:35:02 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
